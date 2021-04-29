8c58f1710055777c884d42811534e54bb1e57953
daiki510 <ddgg7755@gmail.com>
2021-04-29 19:57:49 +0900

    テスト修正

--- test/utils/Utils.test.ts
+++ test/utils/Utils.test.ts
@@ -2,7 +2,7 @@ import * as Utils from '../../src/utils/Utils';
 // import * as Utils from '@/utils/Utils';
 
 describe('fetchNumber', (): void => {
-  test('should say hello to Tom.', (): void => {
+  test('should return a chapter number', (): void => {
       const chapterTitle = 'テスト漫画 - Raw 【第10話】'
       const chapterNumber: string = Utils.fetchNumber(chapterTitle);
       expect(chapterNumber).toBe('10');
